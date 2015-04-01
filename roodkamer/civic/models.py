2f90ce17e223fdad694014919320f2841f089667
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-04-01 17:59:43 -0400

    Added basic article and proposal models

--- roodkamer/civic/models.py
+++ roodkamer/civic/models.py
@@ -8,3 +8,20 @@ from roodkamer.database import (
     relationship,
     SurrogatePK,
 )
+
+class Proposal(SurrogatePK, Model):
+    __tablename__ = "proposals"
+    
+    title = Column(db.String(128), unique=True, nullable=False)
+    body = Column(db.Text)
+    created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
+    last_considered = Column(db.DateTime, nullable=True, default=None)
+    is_live = Column(db.Boolean(), default=False)
+    supporters = relationship('users', backref='proposals')
+    opponents = relationship('users', backref='proposals')
+    
+    def __init__(self, title, **kwargs):
+        db.Model.__init__(self, title=title, **kwargs) 
+
+    def __repr__(self):
+        return '<Proposal({title})>'.format(title=self.title)
--- roodkamer/public/models.py
+++ roodkamer/public/models.py
@@ -9,11 +9,13 @@ from roodkamer.database import (
 
 class Article(SurrogatePK, Model):
     __tablename__ = "articles"
-    def __init__(self, title, **kwargs):
-        db.Model.__init__(self, title=title, **kwargs)
-        title = Column(db.String(128), unique=True, nullable=False)
-        body = Column(db.Text)
-        created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
+    title = Column(db.String(128), unique=True, nullable=False)
+    body = Column(db.Text, nullable=True)
+    created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
+    is_visible = Column(db.Boolean(), default=False)
+
+    def __init__(self, title, visible=False, **kwargs):
+        db.Model.__init__(self, title=title, is_visible=visible, **kwargs)
 
     def __repr__(self):
-        return '<Article({title})>'.format(name=self.title)
+        return '<Article({title})>'.format(title=self.title)
