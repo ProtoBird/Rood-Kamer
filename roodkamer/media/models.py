cfc1e3d735f5f70667cb3daed9477c94d8c938dd
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-04-04 15:56:17 -0400

    added tag class and authors for articles; migrated database

--- roodkamer/media/models.py
+++ roodkamer/media/models.py
@@ -10,15 +10,33 @@ from roodkamer.database import (
 
 import datetime as dt
 
+tags = db.Table('tags',
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+    db.Column('articles_id', db.Integer, db.ForeignKey('articles.id'))
+)
+
+authors = db.Table('authors',
+    db.Column('users_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('articles_id', db.Integer, db.ForeignKey('articles.id'))
+)
+
 class Article(SurrogatePK, Model):
     __tablename__ = "articles"
     title = Column(db.String(128), unique=True, nullable=False)
+    authors = db.relationship('Tag', secondary=tags,
+        backref=db.backref('articles', lazy='dynamic'))
     body = Column(db.Text, nullable=True)
     created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
     is_visible = Column(db.Boolean(), default=False)
+    tags = db.relationship('Tag', secondary=tags,
+        backref=db.backref('articles', lazy='dynamic'))
 
     def __init__(self, title, visible=False, **kwargs):
         db.Model.__init__(self, title=title, is_visible=visible, **kwargs)
 
     def __repr__(self):
         return '<Article({title})>'.format(title=self.title)
+
+class Tag(Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = Column(db.String(64), unique=True, nullable=False)
