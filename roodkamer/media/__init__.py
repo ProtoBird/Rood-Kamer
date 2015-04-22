9db6f1dcdd03e9a200e948f7263a22f311549a13
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-04-22 17:53:44 -0400

    Brought into PEP8 compliance.

--- roodkamer/media/__init__.py
+++ roodkamer/media/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-'''The media module, for the writing and publishing of articles 
+'''The media module, for the writing and publishing of articles
 and other such media.'''
 
 from . import views
