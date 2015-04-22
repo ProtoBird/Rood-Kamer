1d8f27a924b48f865f9af1bfc87f17b17251c0c5
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-04-22 17:48:53 -0400

    Brought into PEP8 compliance.

--- roodkamer/media/models.py
+++ roodkamer/media/models.py
@@ -11,15 +11,18 @@ from roodkamer.database import (
 import datetime as dt
 from roodkamer.user.models import User
 
-tags = db.Table('tags',
+tags = db.Table(
+    'tags',
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
     db.Column('articles_id', db.Integer, db.ForeignKey('articles.id'))
 )
 
-authors = db.Table('authors',
+authors = db.Table(
+    'authors',
     db.Column('users_id', db.Integer, db.ForeignKey('users.id')),
     db.Column('articles_id', db.Integer, db.ForeignKey('articles.id'))
 )
+
 
 class Tag(Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,24 +30,36 @@ class Tag(Model):
 
     def __init__(self, name, **kwargs):
         db.Model.__init__(self, name=name, **kwargs)
-        
+
     def __repr__(self):
         return '<Tag({name})>'.format(name=self.name)
+
 
 class Article(SurrogatePK, Model):
     __tablename__ = "articles"
     title = Column(db.String(128), unique=True, nullable=False)
-    authors = db.relationship('User', secondary=authors,
-                                  backref=db.backref('articles', lazy='dynamic'),
-                                  single_parent=True)
+    authors = db.relationship(
+        'User',
+        secondary=authors,
+        backref=db.backref('articles', lazy='dynamic'),
+        single_parent=True
+    )
     category = Column(db.String(80))
     body = Column(db.Text, nullable=True)
-    created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
+    created_at = Column(
+        db.DateTime,
+        nullable=False,
+        default=dt.datetime.utcnow
+    )
     is_visible = Column(db.Boolean(), default=False)
-    subject_tags = db.relationship('Tag', secondary=tags,
+    subject_tags = db.relationship(
+        'Tag',
+        secondary=tags,
         backref=db.backref('articles', lazy='dynamic'),
         single_parent=True,
-        cascade="all,delete,delete-orphan")
+        cascade="all,delete,delete-orphan"
+    )
+
     def __init__(self, title, publish=False, **kwargs):
         db.Model.__init__(self, title=title, is_visible=publish, **kwargs)
 
