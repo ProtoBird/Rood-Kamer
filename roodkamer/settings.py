5bede18184a4eacd0e78204d5b31da8faa24dfaa
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-04-24 12:22:18 -0400

    Brought into pep8 compliance

--- roodkamer/settings.py
+++ roodkamer/settings.py
@@ -3,6 +3,7 @@ import os
 
 os_env = os.environ
 
+
 class Config(object):
     SECRET_KEY = os_env.get('RK_SKI', 'secret-key')  # TODO: Change me
     RK_ADMIN = os_env.get('RK_ADMIN')
@@ -14,12 +15,12 @@ class Config(object):
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
 
-    
+
 class ProdConfig(Config):
     """Production configuration."""
     ENV = 'prod'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/example'  # TODO: Change me
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/example'  # TODO: Change
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
 
 
@@ -27,29 +28,27 @@ class DevConfig(Config):
     """Development configuration."""
     ENV = 'dev'
     DEBUG = True
-    
+
     SQLALCHEMY_ECHO = False
     DEBUG_TB_ENABLED = True
     ASSETS_DEBUG = True  # Don't bundle/minify static assets
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
-    
-    RK_DB = os_env.get('RK_DB')    
+
+    RK_DB = os_env.get('RK_DB')
     RK_DB_PASSWORD = os_env.get('RK_DB_PASSWORD')
     RK_DB_HOST = 'localhost'
-    
-    SQLALCHEMY_DATABASE_URI = "mysql://%s:%s@%s:3306/%s" % (
-                                                  Config.RK_ADMIN,
-                                                  RK_DB_PASSWORD,
-                                                  RK_DB_HOST,
-                                                  RK_DB
-                                                  )
+
+    SQLALCHEMY_DATABASE_URI = "mysql://%s:%s@%s:3306/%s" % (Config.RK_ADMIN,
+                                                            RK_DB_PASSWORD,
+                                                            RK_DB_HOST,
+                                                            RK_DB)
 
 
 class TestConfig(Config):
     """Testing configuration"""
 
     DB_NAME = "test.db"
-    SQLALCHEMY_DATABASE_URI = "sqlite://" 
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
     TESTING = True
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 1  # For faster tests
