2f947609ed1c347b932845f92c74ccd4e354eace
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-07-04 18:27:13 -0400

    Added detailed docstrings and changed the depriciated TextField to StringField.

--- roodkamer/media/forms.py
+++ roodkamer/media/forms.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import Form
-from wtforms import TextField, PasswordField, SubmitField
+from wtforms import TextField
 from wtforms.widgets import TextArea
 from wtforms.fields import TextAreaField, SelectMultipleField, SelectField
 from wtforms.validators import DataRequired, Length
-from wtforms.fields.core import BooleanField
+from wtforms.fields.core import BooleanField, StringField
+from wtforms.fields.simple import SubmitField
 
 from roodkamer.user.models import User
 from roodkamer.media.models import Article
@@ -14,7 +15,7 @@ class CKTextAreaWidget(TextArea):
     """Widget for displaying WYSIWYG CK Editor
     """
     def __call__(self, field, **kwargs):
-        kwargs.setdefault('class_', 'ckeditor')
+        kwargs.setdefault('class_', 'ckeditorCKTextAreaField')
         return super(CKTextAreaWidget, self).__call__(field, **kwargs)
  
  
@@ -37,10 +38,34 @@ class ArticleForm(Form):
     Form for writing new articles and editing published articles.
     
     Attributes:
-    
+        title (:class:`~wtforms.fields.StringField`): This is the title field. It is
+            required, and must contain no less than 3 characters and no more
+            than 128. Title is validated for uniqueness.
+        authors (:class:`~wtforms.fields.SelectMultipleField`): This is the 
+            field which specifies the author and co-authors of the article. The
+            selections are based on usernames, with the first listed considered
+            the author and the rest are considered co-authors. All users listed
+            as authors have permission to edit the article. At least one author
+            is required.
+        body (:class:`~roodkamer.media.forms.CKTextAreaField`): This is the
+            field for the article text and other media.
+        category (:class:`wtforms.fields.SelectField`): This is the field for
+            selecting which category an article belongs in. Categories may be
+            selected from database of existing articles or a new one can be 
+            inserted on article creation. This field is required.
+        post (:class:`wtforms.fields.SubmitField`): Submit button. For submitting an
+            article.
+        cancel (:class:`wtforms.fields.SubmitField`): Cancel button. For not submitting 
+            an article.
+        is_visible (:class:`~wtforms.fields.BooleanField`): A checkbox for
+            determining if the article is displayed on the site.
+    subject_tags (:class:`~wtforms.fields.StringField`): A text input modified with
+            bootrap-tagsinput script for any number of identifying tags to be 
+            associated with the article.
+        
     """
-    title = TextField('Title',
-                      validators=[DataRequired(), Length(min=3, max=128)])
+    title = StringField('Title',
+                        validators=[DataRequired(), Length(min=3, max=128)])
     authors = SelectMultipleField('Author(s)', validators=[DataRequired()],
                                   coerce=int)
     body = CKTextAreaField("Body")
@@ -48,7 +73,7 @@ class ArticleForm(Form):
                            validators=[DataRequired(), Length(min=3, max=80)])
     post = SubmitField("Post")
     is_visible = BooleanField("Publish")
-    subject_tags = TextField("Tags")
+    subject_tags = StringField("Tags")
     cancel = SubmitField("Cancel")
 
     def __init__(self, *args, **kwargs):
@@ -58,6 +83,8 @@ class ArticleForm(Form):
         initial_validation = super(ArticleForm, self).validate()
         if not initial_validation:
             return False
+        
+        # Make sure that title is unique
         article = Article.query.filter_by(title=self.title.data).first()
         if article:
             self.title.errors.append("Title already in use")
