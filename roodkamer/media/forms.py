82861528d4e553c663553bf6acea3de4fbaaa434
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2015-07-05 21:26:58 -0400

    Brought into pep8 compliance.

--- roodkamer/media/forms.py
+++ roodkamer/media/forms.py
@@ -17,31 +17,32 @@ class CKTextAreaWidget(TextArea):
     def __call__(self, field, **kwargs):
         kwargs.setdefault('class_', 'ckeditorCKTextAreaField')
         return super(CKTextAreaWidget, self).__call__(field, **kwargs)
- 
- 
+
+
 class CKTextAreaField(TextAreaField):
     """Text area field WYSIWYG CK Editor
-    
+
     Text area for plugging the `roodkamer.media.CKTextAreaWidget`
-    into a `flask_wtf.Form`.
-    
+    into a :class:`~flask_wtf.Form`.
+
     Attributes:
-        widget (:class:`~roodkamer.media.forms.CKTextAreaWidget`): Pluggin location for the CK Text
-            widget.
-    
+        widget (:class:`~roodkamer.media.forms.CKTextAreaWidget`): Pluggin
+            location for the CK Text widget.
+
     """
     widget = CKTextAreaWidget()
 
+
 class ArticleForm(Form):
     """Article Form
-    
+
     Form for writing new articles and editing published articles.
-    
+
     Attributes:
-        title (:class:`~wtforms.fields.StringField`): This is the title field. It is
-            required, and must contain no less than 3 characters and no more
-            than 128. Title is validated for uniqueness.
-        authors (:class:`~wtforms.fields.SelectMultipleField`): This is the 
+        title (:class:`~wtforms.fields.StringField`): This is the title field.
+            It is required, and must contain no less than 3 characters and no
+            more than 128. Title is validated for uniqueness.
+        authors (:class:`~wtforms.fields.SelectMultipleField`): This is the
             field which specifies the author and co-authors of the article. The
             selections are based on usernames, with the first listed considered
             the author and the rest are considered co-authors. All users listed
@@ -51,18 +52,18 @@ class ArticleForm(Form):
             field for the article text and other media.
         category (:class:`wtforms.fields.SelectField`): This is the field for
             selecting which category an article belongs in. Categories may be
-            selected from database of existing articles or a new one can be 
+            selected from database of existing articles or a new one can be
             inserted on article creation. This field is required.
-        post (:class:`wtforms.fields.SubmitField`): Submit button. For submitting an
-            article.
-        cancel (:class:`wtforms.fields.SubmitField`): Cancel button. For not submitting 
-            an article.
+        post (:class:`wtforms.fields.SubmitField`): Submit button. For
+            submitting an article.
+        cancel (:class:`wtforms.fields.SubmitField`): Cancel button. For not
+            submitting an article.
         is_visible (:class:`~wtforms.fields.BooleanField`): A checkbox for
             determining if the article is displayed on the site.
-    subject_tags (:class:`~wtforms.fields.StringField`): A text input modified with
-            bootrap-tagsinput script for any number of identifying tags to be 
-            associated with the article.
-        
+        subject_tags (:class:`~wtforms.fields.StringField`): A text input
+            modified with bootrap-tagsinput script for any number of
+            identifying tags to be associated with the article.
+
     """
     title = StringField('Title',
                         validators=[DataRequired(), Length(min=3, max=128)])
@@ -83,12 +84,11 @@ class ArticleForm(Form):
         initial_validation = super(ArticleForm, self).validate()
         if not initial_validation:
             return False
-        
+
         # Make sure that title is unique
         article = Article.query.filter_by(title=self.title.data).first()
         if article:
             self.title.errors.append("Title already in use")
             return False
         else:
-            return True
-
+            return True
