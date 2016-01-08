1096a4d935dfd8dd6e0efdb792a17e40895d4fb9
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2016-01-08 16:01:32 -0500

    Library front end that does not explode or do anything really.

--- roodkamer/library/views.py
+++ roodkamer/library/views.py
@@ -3,6 +3,25 @@ from flask import Blueprint, render_template
 from flask.ext.login import login_required
 
 from roodkamer.library.models import Book, Author, Publisher
+from roodkamer.user.models import User, Permission
+from roodkamer.decorators import permissions_required
 
 blueprint = Blueprint("library", __name__, url_prefix='/library',
                       static_folder="../static")
+
+from flask.views import MethodView
+from flask.ext.login import login_required
+
+
+class LibraryMain(MethodView):
+    decorators = [login_required]
+
+    @permissions_required(Permission.CHECK_OUT_BOOK)
+    def get(self):
+        return render_template("library/main.html")
+
+
+blueprint.add_url_rule(
+    "/main/", 
+    view_func=LibraryMain.as_view("main"))
+    
